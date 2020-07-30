9527139acb23945b431f859cd6b0aaa2b85fd93a
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-07-30 16:28:19 -0400

    set up basics for card game

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 
 //: ## Step 1
 //: Create an enumeration for the value of a playing card. The values are: `ace`, `two`, `three`, `four`, `five`, `six`, `seven`, `eight`, `nine`, `ten`, `jack`, `queen`, and `king`. Set the raw type of the enum to `Int` and assign the ace a value of `1`.
-enum cards: Int{
+enum Cards: Int{
     case ace = 1
     case two = 2
     case three = 3
@@ -13,9 +13,11 @@ enum cards: Int{
     case eight = 8
     case nine = 9
     case ten = 10
-    case jack = 11
-    case queen = 12
-    case king = 13
+    case jack = 13
+    case queen = 14
+    case king = 15
+    
+    static var allRanks: [Cards] = [.ace, .two, .three, .four, .five, .six, .seven, .eight, .nine, .ten, .jack, .queen, .king]
 }
 
 
@@ -23,30 +25,71 @@ enum cards: Int{
 
 //: ## Step 2
 //: Once you've defined the enum as described above, take a look at this built-in protocol, [CustomStringConvertible](https://developer.apple.com/documentation/swift/customstringconvertible) and make the enum conform to that protocol. Make the face cards return a string of their name, and for the numbered cards, simply have it return that number as a string.
-
-
-
+extension Cards: CustomStringConvertible {
+    var description: String{
+        switch self {
+        case .ace:
+            return "Ace"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
 
 //: ## Step 3
 //: Create an enum for the suit of a playing card. The values are `hearts`, `diamonds`, `spades`, and `clubs`. Use a raw type of `String` for this enum (this will allow us to get a string version of the enum cases for free, no use of `CustomStringConvertible` required).
-
+enum suit: String{
+    case hearts
+    case diamonds
+    case spades
+    case clubs
+    
+    static var allSuits: [suit] = [.hearts, .diamonds, .spades, .clubs]
+}
 
 
 
 //: ## Step 4
 //: Using the two enums above, create a `struct` called `Card` to model a single playing card. It should have constant properties for each constituent piece (one for suit and one for rank).
-
+struct Card {
+    let rank: Cards
+    let suits: suit
+}
 
 
 
 //: ## Step 5
 //: Make the card also conform to `CustomStringConvertible`. When turned into a string, a card's value should look something like this, "ace of spades", or "3 of diamonds".
-
+extension Card: CustomStringConvertible {
+    var description: String{
+        return "\(self.rank) of \(suits)"
+    }
+}
 
 
 //: ## Step 6
 //: Create a `struct` to model a deck of cards. It should be called `Deck` and have an array of `Card` objects as a constant property. A custom `init` function should be created that initializes the array with a card of each rank and suit. You'll want to iterate over all ranks, and then over all suits (this is an example of _nested `for` loops_). See the next 2 steps before you continue with the nested loops.
-
+struct Deck{
+    var cards: [Card] = []
+    
+    init() {
+        for ranking in Cards.allRanks{
+            for suitInCard in suit.allSuits{
+                cards.append(Card(rank: ranking, suits: suitInCard))
+            }
+        }
+    }
+    func drawCard() -> Card {
+        let position = Int.random(in: 0...cards.count - 1)
+        return cards[position]
+    }
+}
 
 
 
@@ -92,7 +135,10 @@ enum cards: Int{
 //: Create a protocol for a `CardGame`. It should have two requirements:
 //: * a gettable `deck` property
 //: * a `play()` method
-
+protocol CardGame {
+    var deck: Deck {get}
+    func play()
+}
 
 
 
@@ -101,7 +147,10 @@ enum cards: Int{
 //: * a function called `gameDidStart` that takes a `CardGame` as an argument
 //: * a function with the following signature: `game(player1DidDraw card1: Card, player2DidDraw card2: Card)`
 
-
+protocol CardGameDelegate {
+    func gameDidStart(CardGame: CardGame)
+    func game(player1DidDraw card1: Card, player2DidDraw card2: Card)
+}
 
 
 //: ## Step 14
